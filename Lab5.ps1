8bc797b751fa9e0b460f04f1e0906dbe9dcf4474
Laxmi200476514 <106429782+Laxmi200476514@users.noreply.github.com>
2022-08-18 16:26:52 -0400

    Add files via upload
    Lab-5 Powershell

---
+++ Lab5.ps1
@@ -0,0 +1,35 @@
+﻿Param(
+   [Parameter(position=1)]
+   [switch]$system ,
+  
+   [Parameter(Position=2)]
+   [switch]$disk ,
+ 
+   [Parameter(Position=3)]
+   [switch]$network
+
+)
+
+
+
+if ($system -ne $true -and $disk -ne $true -and $network -ne $true) 
+{
+    welcome
+    cpuinfo
+    get-mydisks
+    get-system
+    get-network
+    get-disk
+}
+
+elseif ($disk -eq $true) {
+    get-disk
+}
+
+elseif ($network -eq $true) {
+    get-network
+}
+
+elseif ($system -eq $true) {
+    get-system
+}
